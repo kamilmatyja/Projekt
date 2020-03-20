cac71fe802067bd8eed046f3a7e48e190b2bdfff
Kamil Matyja <57847623+kamilmatyja@users.noreply.github.com>
2020-03-20 22:09:32 +0100

    Update scrollup.js

--- scrollup.js
+++ scrollup.js
@@ -1,31 +1,26 @@
+var scrollButton = document.querySelector('#scrollup');
 
-var basicScrollTop = function () 
-{  
-  var btnTop = document.querySelector('#scrollup');
-
-  var btnReveal = function () 
-  { 
-    if (window.scrollY >= 150) {
-      btnTop.classList.add('is-visible');
-    } else {
-      btnTop.classList.remove('is-visible');
+window.addEventListener('scroll', function() 
+{
+    if(window.scrollY > 150)
+	{
+	    scrollButton.classList.remove('un-visible');
+        scrollButton.classList.add('is-visible');
+    } 
+	else
+	{
+        scrollButton.classList.remove('is-visible');
+	    scrollButton.classList.add('un-visible');
     }    
-  }  
+});  
 
-  var TopscrollTo = function () 
-  {
-    if(window.scrollY!=0) {
-      setTimeout(function() {
-        window.scrollTo(0,window.scrollY-0);
-        TopscrollTo();
+scrollButton.addEventListener('click', function() 
+{
+    if(window.scrollY != 0) 
+	{
+      setTimeout(function() 
+	  {
+        window.scrollTo(0, window.scrollY);
       }, 50000);
     }
-  }
-
-  window.addEventListener('scroll', btnReveal);
-  btnTop.addEventListener('click', TopscrollTo);  
-};
-basicScrollTop();
-
-
-
+});
